b44fd36b0fd64ad1df5ea6ccd02866507e6d1dd3
Kevin McLoughlin <kevinmcloughlin2010@gmail.com>
2015-09-11 01:01:00 -0400

    transform each hash into a Ruby object, and update view logic accordingly

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,9 @@
-class Photo < ActiveRecord::Base
+class Photo
   include HTTParty
   base_uri 'https://api.500px.com/v1'
+
+  attr_accessor :id, :name, :width, :height, :for_sale, :nsfw,
+    :image_url, :author_full_name, :author_image
 
   def self.api_key
     Figaro.env.consumer_key
@@ -13,6 +16,21 @@ class Photo < ActiveRecord::Base
     # build query using the selected feature and sorting options
     composed_url = "/photos?#{ feature }#{ sorting }&consumer_key=#{ api_key }"
 
-    get(composed_url)['photos']
+    photos = get(composed_url)['photos']
+
+    # transform each hash into a Ruby object
+    photos.map{ |photo| Photo.new(photo) }
+  end
+
+  def initialize(params)
+    @id = params['id']
+    @name = params['name']
+    @width = params['width']
+    @height = params['height']
+    @for_sale = params['for_sale']
+    @nsfw = params['nsfw']
+    @image_url = params['image_url']
+    @author_full_name = params['user']['fullname']
+    @author_image = params['user']['userpic_url']
   end
 end
