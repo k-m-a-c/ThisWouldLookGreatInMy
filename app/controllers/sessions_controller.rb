b7545e8f326aa0ac0a9c0bd987956ce97ae1b6a2
Kevin McLoughlin <kevinmcloughlin2010@gmail.com>
2015-09-14 07:21:29 -0400

    require sign up and redirect to Photos upon success

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,12 @@
 class SessionsController < ApplicationController
+
+  # followed Railscast PRO #241: Simple OmniAuth
+  # episode to set up OAuth 1.0a sign in feature
+  # https://www.youtube.com/watch?v=KOnRf1K6Eg8
   def create
     user = User.from_omniauth(env['omniauth.auth'])
     session[:user_id] = user.id
-    redirect_to root_url, notice: "Signed in!"
+    redirect_to photos_url, notice: "Signed in!"
   end
 
   def destroy
